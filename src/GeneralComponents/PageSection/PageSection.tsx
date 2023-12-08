a23998b0de6967964baa27bb15e8b4a875771a8a
JakeSaintG <jakesaint27@gmail.com>
2023-12-08 18:08:47 -0500

    start working through light/dark mode

--- src/GeneralComponents/PageSection/PageSection.tsx
+++ src/GeneralComponents/PageSection/PageSection.tsx
@@ -8,13 +8,16 @@ export interface Props extends React.HTMLAttributes<HTMLSelectElement>{
     id: string
 }
 
+// flip this to style.dark_mode_content
+let colorMode = styles.light_mode_content;
+
 export default function PageSection( props: Props) {
     const { children, style, title, id} = props
     
     return (
         <section className={styles.page_section} id={id} >
             <h1>{title}</h1>
-            <div className={styles.content} style={style}>
+            <div className={`${styles.content} ${colorMode}`} style={style}>
                 {children}
             </div>
         </section>
