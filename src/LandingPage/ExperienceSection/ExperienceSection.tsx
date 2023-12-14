ec40dcae6b3958ae6345f0624ff43d5a5f393b19
JakeSaintG <jakesaint27@gmail.com>
2023-12-13 22:53:20 -0500

    experience styling updates

--- src/LandingPage/ExperienceSection/ExperienceSection.tsx
+++ src/LandingPage/ExperienceSection/ExperienceSection.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { MouseEvent, useState } from 'react';
 import PageSection from '../../GeneralComponents/PageSection';
 import styles from './ExperienceSection.module.css';
 
@@ -60,30 +60,30 @@ const workExperience = [
 
 export default function ExperienceSection(props: Props) {
     const { visualMode } = props;
-    let [bar, setBar] = useState(workExperience[0].highlights);
+    let [workHighlights, setWorkHighlights] = useState(workExperience[0].highlights);
     let key = 0;
 
     visualMode === 'dark_mode' ? detailsVisualMode = styles.dark_mode_details : detailsVisualMode = styles.light_mode_details;
 
-    const returnWorkExperience = (careerId: string) => {
-        setBar(bar = workExperience.find(e => e.title === careerId)!.highlights);
+    const returnWorkExperience = (event: MouseEvent<HTMLElement>) => {
+        setWorkHighlights(workHighlights = workExperience.find(e => e.title === event.currentTarget.id)!.highlights);
     }
 
     return (
         <PageSection title='Experience' id="experienceSection" style={contentStyle} visualMode={visualMode}>
             <div className={`${styles.experience_details} ${detailsVisualMode}`}>
-                <ul>{bar.map(e => <li key={key++}>{e}</li>)}</ul>
+                <ul>{workHighlights.map(e => <li key={key++}>{e}</li>)}</ul>
             </div>
             <div className={styles.experience}>
                 <h3>Current:</h3>
-                <button onClick={() => returnWorkExperience('dev-fulltime')}>Software Developer</button>
-                <button onClick={() => returnWorkExperience('mentor')}>Code Louisville Mentor</button>
+                <button id='dev-fulltime' onClick={returnWorkExperience}>Software Developer</button>
+                <button id='mentor' onClick={returnWorkExperience}>Code Louisville Mentor</button>
                 
                 <h3>Previous:</h3>
-                <button onClick={() => returnWorkExperience('dev-intern')}>Software Developer Intern</button>
-                <button onClick={() => returnWorkExperience('dev-student')}>Code Louisville Student</button>
-                <button onClick={() => returnWorkExperience('nurse')}>ICU Nurse</button>
-                <button onClick={() => returnWorkExperience('awards')}>Awards</button>
+                <button id='dev-intern' onClick={returnWorkExperience}>Software Developer Intern</button>
+                <button id='dev-student' onClick={returnWorkExperience}>Code Louisville Student</button>
+                <button id='nurse' onClick={returnWorkExperience}>ICU Nurse</button>
+                <button id='awards' onClick={returnWorkExperience}>Awards</button>
             </div>
         </PageSection>
     );
