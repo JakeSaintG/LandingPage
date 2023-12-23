2d395fc788eae82d5050918d5e9a9cdbc0f06c59
JakeSaintG <jakesaint27@gmail.com>
2023-12-23 17:47:17 -0500

    Start working through projects display

--- src/LandingPage/ProjectsSection/ProjectsSection.tsx
+++ src/LandingPage/ProjectsSection/ProjectsSection.tsx
@@ -5,14 +5,38 @@ interface Props extends React.HTMLAttributes<HTMLElement>{
     visualMode: string
 }
 
-const contentStyle: React.CSSProperties = { };
+const projects = [
+    {
+        name: "test",
+        hosted: null,
+        technologies: [
+            "C#"
+        ]
+    },
+    {
+        name: "test2",
+        hosted: "gh pages link",
+        technologies: [
+            "Angular"
+        ]
+    }
+];
+
+const contentStyle: React.CSSProperties = { display: 'grid', gridTemplateColumns:'1fr' };
 
 export default function ProjectsSection(props: Props) {
     const { visualMode } = props;
+    let key = 0;
     
     return (
         <PageSection title='Projects' id="projectsSection" style={contentStyle} visualMode={visualMode}>
-            <p>Placeholder</p>
+            <div className={styles.projects}>
+                {projects.map(e =>
+                    <div key={key++} className={styles.project}>
+                        {e.name}
+                    </div>)
+                }
+            </div>
         </PageSection>
     );
 }
