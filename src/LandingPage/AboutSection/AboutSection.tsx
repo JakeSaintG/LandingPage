fae5fcff6e47df1877c7de960be446026df32b85
JakeSaintG <jakesaint27@gmail.com>
2024-01-10 19:09:50 -0500

    bug fix for tidbit

--- src/LandingPage/AboutSection/AboutSection.tsx
+++ src/LandingPage/AboutSection/AboutSection.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState, useSyncExternalStore } from 'react';
 import PageSection from '../../GeneralComponents/PageSection';
 import styles from './AboutSection.module.css';
 import tidbits from '../../assets/tidbits.json'
@@ -11,19 +11,18 @@ interface Props extends React.HTMLAttributes<HTMLElement>{
 const contentStyle: React.CSSProperties = { };
 
 const tibbitAnimation = [
-    { transform: "translateX(-450px)" },
+    { transform: "translateX(-550px)" },
     { transform: 'translateX(0)', offset: 0.15 },
     { transform: 'translateX(0)', offset: 0.85 },
-    { transform: "translateX(450px)" }
+    { transform: "translateX(550px)" }
 ];
 
-let intervalSet = false;
-
 const useInterval = (callback: Function, delay: number) => {
-    if (!intervalSet) {
+    let [test, setTest] = useState(false);
+    
+    if (!test) {
         setInterval(callback, delay);
-        intervalSet = true;
-        console.log("wut")
+        setTest(test = true);
     }
 }
 
@@ -43,14 +42,9 @@ export default function AboutSection(props: Props) {
             newTidbit = tidbits[Math.floor(Math.random() * tidbits.length)];
         } while (newTidbit === tidbit);
 
-        // If the text animation was left to run, it would get out of sync in a few minutes.
-        // This (hopefully) ensures that it stays in sync.
-        tidbitRef.current?.animate(tibbitAnimation, {duration: 3000}).commitStyles();
+        tidbitRef.current?.animate(tibbitAnimation, {duration: 4000}).commitStyles();
         setTidbit(tidbit = newTidbit);
-
-        console.log("double wut");
     }, 4000);
-
 
     return (
         <PageSection title='Jake St. Germain' id="aboutSection" style={contentStyle} visualMode={visualMode}>
@@ -61,7 +55,6 @@ export default function AboutSection(props: Props) {
                         {elevatorPitch.map(e => 
                             <p key={key++} className={styles.project}>
                                 {e}
-                                
                             </p>
                         )}
                     </div>
