31949e623d9289326e44a68b0f97f0a2110e3042
JakeSaintG <jakesaint27@gmail.com>
2024-12-22 17:50:42 -0700

    updating settings

--- src/GeneralComponents/SlideToggle/SlideToggle.tsx
+++ src/GeneralComponents/SlideToggle/SlideToggle.tsx
@@ -1,5 +1,7 @@
 import { ChangeEventHandler } from 'react';
 import styles from './SlideToggle.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 
 interface Props extends React.HTMLAttributes<HTMLElement>{
     value: string
@@ -8,11 +10,20 @@ interface Props extends React.HTMLAttributes<HTMLElement>{
 }
 
 let sliderColorMode: string;
+let icon: JSX.Element;
 
 export default function SlideToggle( props: Props ) {
     const { value, onChange, visualMode } = props
 
-    visualMode === 'dark_mode' ? sliderColorMode = styles.slider_dark : sliderColorMode = styles.slider_light;
+    // visualMode === 'dark_mode' ? sliderColorMode = styles.slider_dark : sliderColorMode = styles.slider_light;
+
+    if (visualMode === 'dark_mode') {
+        sliderColorMode = styles.slider_dark
+        icon  = <FontAwesomeIcon icon={faMoon} size='xl' />
+    } else {
+        sliderColorMode = styles.slider_light
+        icon  = <FontAwesomeIcon icon={faSun} size='xl'  />
+    }
     
     //TODO: Make props that allow for passing in svg files for the icons
     return (
@@ -21,7 +32,9 @@ export default function SlideToggle( props: Props ) {
                 <input id="switch" type="checkbox" value={value} onChange={onChange} />
                 <div className={styles.button}>
                 <span className={`${styles.button_toggle} ${sliderColorMode}`}></span>
-                <span className={styles.button_indicator}></span>
+                <span className={styles.button_indicator}>
+                    {icon}
+                </span>
                 </div>
             </div>
         </label>
