7ada1100aef7f2be5f6725e52d304320a5bf0774
JakeSaintG <jakesaint27@gmail.com>
2024-12-24 17:54:36 -0700

    pass down header info (missed in prev commit)

--- src/GeneralComponents/PageSection/PageSection.tsx
+++ src/GeneralComponents/PageSection/PageSection.tsx
@@ -7,18 +7,25 @@ interface Props extends React.HTMLAttributes<HTMLElement>{
     title: string
     id: string
     visualMode: string
+    header?: string
 }
 
 let colorMode: string;
 
 export default function PageSection( props: Props ) {
-    const { children, style, title, id, visualMode } = props;
+    const { children, style, title, id, visualMode, header = 'h2' } = props;
 
     visualMode === 'dark_mode' ? colorMode = styles.dark_mode_content : colorMode = styles.light_mode_content;
+    let h = (<h2 className={styles.section_title}>{title}</h2>);
+
+
+    if (header === 'h1') {
+        h = (<h1 className={styles.section_title}>{title}</h1>);
+    }
 
     return (
         <section className={styles.page_section} id={id} >
-            <h1 className={styles.section_title}>{title}</h1>
+            {h}
             <div className={`${styles.content} ${colorMode}`} style={style}>
                 {children}
             </div>
