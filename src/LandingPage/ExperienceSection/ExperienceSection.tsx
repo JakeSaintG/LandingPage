46289f4568f7b40202ee07de2791ea83e5419153
JakeSaintG <jakesaint27@gmail.com>
2025-01-12 23:47:09 -0500

    refactor how experience section uses the experience json

--- src/LandingPage/ExperienceSection/ExperienceSection.tsx
+++ src/LandingPage/ExperienceSection/ExperienceSection.tsx
@@ -13,10 +13,23 @@ export default function ExperienceSection(props: Props) {
     const { visualMode } = props;
     let [workHighlights, setWorkHighlights] = useState(workExperience[0].highlights);
     let [activeClass, setActiveClass] = useState('dev-fulltime');
-    let key = 0;
+    let details_key = 0;
+    let previous_key = 0;
+    let current_key = 0;
+
+    // TODO: Do these need to be in a useState()? Probably?
+    const currentWorkExp = workExperience.reduce( (acc, work) => {
+        if(work.current) acc.push(work);
+        return acc;
+    }, [] as any[] );
+
+    const prevWorkExp = workExperience.reduce( (acc, work) => {
+        if(!work.current) acc.push(work);
+        return acc;
+    }, [] as any[] );
 
     const returnWorkExperience = (event: MouseEvent<HTMLElement>) => {
-        setWorkHighlights(workHighlights = workExperience.find(e => e.title === event.currentTarget.id)!.highlights);
+        setWorkHighlights(workHighlights = workExperience.find(e => e.id === event.currentTarget.id)!.highlights);
         setActiveClass(activeClass = event.currentTarget.id);
     }
 
@@ -24,57 +37,35 @@ export default function ExperienceSection(props: Props) {
         <PageSection title='Experience' id="experienceSection" style={contentStyle} visualMode={visualMode}>
             <div className={styles.experience_content}>
                 <div className={`${styles.experience_details} ${styles[props.visualMode]}`}>
-                    <ul>{workHighlights.map(e => <li key={key++}>{e}</li>)}</ul>
+                    <ul>{workHighlights.map(e => <li key={details_key++}>{e}</li>)}</ul>
                 </div>
                 <div className={styles.experience}>
                     <h3 className={styles.current_header}>Current:</h3>
                     <div className={styles.current_experience}>
-                        <button 
-                            className={`${styles[props.visualMode]} ${activeClass === 'dev-fulltime' ? `${styles.selected_profession} ${styles[props.visualMode]}` : `${styles.profession}`}`} 
-                            id='dev-fulltime'
-                            onClick={returnWorkExperience}
-                        >
-                            Software Developer
-                        </button>
-                        <button 
-                            className={`${styles[props.visualMode]} ${activeClass === 'mentor' ? `${styles.selected_profession} ${styles[props.visualMode]}` : `${styles.profession}`}`} 
-                            id='mentor' 
-                            onClick={returnWorkExperience}
-                        >
-                            Code Louisville Mentor
-                        </button>
+                        {currentWorkExp.map((e: any) =>
+                            <button 
+                                key={previous_key++}
+                                className={`${styles[props.visualMode]} ${activeClass === e.id ? `${styles.selected_profession} ${styles[props.visualMode]}` : `${styles.profession}`}`}
+                                id={e.id}
+                                onClick={returnWorkExperience}
+                            >
+                                    {e.title}
+                            </button>
+                        )}
                     </div>
 
                     <h3 className={styles.past_header}>Previous:</h3>
                     <div className={styles.past_experience}>
-                        <button 
-                            className={`${styles[props.visualMode]} ${activeClass === 'dev-intern' ? `${styles.selected_profession} ${styles[props.visualMode]}` : `${styles.profession}`}`}
-                            id='dev-intern' 
-                            onClick={returnWorkExperience}
-                        >
-                                Software Dev Intern
-                        </button>
-                        <button 
-                            className={`${styles[props.visualMode]} ${activeClass === 'dev-student' ? `${styles.selected_profession} ${styles[props.visualMode]}` : `${styles.profession}`}`}
-                            id='dev-student' 
-                            onClick={returnWorkExperience}
-                        >
-                                Code Louisville Student
-                        </button> 
-                        <button 
-                            className={`${styles[props.visualMode]} ${activeClass === 'nurse' ? `${styles.selected_profession} ${styles[props.visualMode]}` : `${styles.profession}`}`}
-                            id='nurse' 
-                            onClick={returnWorkExperience}
-                        >
-                                ICU Nurse
-                        </button>
-                        <button 
-                            className={`${styles[props.visualMode]} ${activeClass === 'awards' ? `${styles.selected_profession} ${styles[props.visualMode]}` : `${styles.profession}`}`}
-                            id='awards' 
-                            onClick={returnWorkExperience}
-                        >
-                                Awards
-                        </button>
+                        {prevWorkExp.map((e: any) =>
+                            <button 
+                                key={previous_key++}
+                                className={`${styles[props.visualMode]} ${activeClass === e.id ? `${styles.selected_profession} ${styles[props.visualMode]}` : `${styles.profession}`}`}
+                                id={e.id}
+                                onClick={returnWorkExperience}
+                            >
+                                    {e.title}
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
