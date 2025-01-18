c117084b5236450f38d91e7175becaec86dc6bd3
JakeSaintG <jakesaint27@gmail.com>
2025-01-18 12:06:39 -0500

    finish adding code katas, work through project style

--- src/LandingPage/ProjectsSection/Project/Project.tsx
+++ src/LandingPage/ProjectsSection/Project/Project.tsx
@@ -5,6 +5,9 @@ import angularImg from '../../../assets/img/lang_icons/angular.png';
 import htmlImg from '../../../assets/img/lang_icons/html.png';
 import javascriptImg from '../../../assets/img/lang_icons/javascript.png';
 import typescriptImg from '../../../assets/img/lang_icons/typescript.png';
+import powershellImg from '../../../assets/img/lang_icons/powershell.png';
+import dartImg from '../../../assets/img/lang_icons/dart.png';
+import rustImg from '../../../assets/img/lang_icons/rust.png';
 import sqlImg from '../../../assets/img/lang_icons/sql.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
@@ -29,7 +32,10 @@ const langs: Record<string, string> = {
     typescript: typescriptImg,
     html: htmlImg,
     angular: angularImg,
-    sql: sqlImg
+    sql: sqlImg,
+    powershell: powershellImg,
+    dart: dartImg,
+    rust: rustImg
 }
 
 export default function Project(props: Props) {
@@ -38,13 +44,12 @@ export default function Project(props: Props) {
 
     if (props.project.hosted) {
         hosted = <a href={props.project.hosted} target='_blank'>
-            <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="lg" />
+            <FontAwesomeIcon icon={faArrowUpRightFromSquare} size="xl" />
         </a>
     }
 
     return (
         <div className={`${styles.project} ${styles[props.visualMode]}`}>
-
             <div className={styles.project_header}>
                 <h3>{props.project.name}</h3>
                 <div>
@@ -52,12 +57,11 @@ export default function Project(props: Props) {
                         return <img key={key++} src={langs[t]} alt={`Icon for ${t}`} />;
                     })}
                 </div>
-
             </div>
             <p>{props.project.description}</p>
             <span>
                 <a href={props.project.repo_link} target='_blank'>
-                    <FontAwesomeIcon icon={faGithub} size="lg" />
+                    <FontAwesomeIcon icon={faGithub} size="xl" />
                 </a>
                 {hosted}
             </span>
