b8320b8cfec4a503392a100abadc175a10143576
JakeSaintG <jakesaint27@gmail.com>
2025-02-16 00:44:11 -0500

    start working through mobile nav

--- src/Navbar/Navbar.tsx
+++ src/Navbar/Navbar.tsx
@@ -18,44 +18,47 @@ export default function Navbar(props: Props) {
         <nav className={styles[props.visualMode]}>
             <div className={styles.nav_logo}>Logo</div>
 
-            <a className={styles.nav_item} href="#aboutSection">
-                About
-            </a>
-            <a className={styles.nav_item} href="#experienceSection">
-                Experience
-            </a>
-            <a className={styles.nav_item} href="#projectsSection">
-                Projects
-            </a>
-            <a className={styles.nav_item} href="#projectsSection">
-                Blog
-            </a>
+            <div className={styles.nested_nav}>
 
-            <div className={styles.socials}>
-                <a href="https://www.linkedin.com/in/jake-st-germain/">
-                    <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
+                <a className={styles.nav_item} href="#aboutSection">
+                    About
+                </a>
+                <a className={styles.nav_item} href="#experienceSection">
+                    Experience
+                </a>
+                <a className={styles.nav_item} href="#projectsSection">
+                    Projects
+                </a>
+                <a className={styles.nav_item} href="#projectsSection">
+                    Blog
                 </a>
 
-                <a href="https://github.com/JakeSaintG">
-                    <FontAwesomeIcon icon={faGithub} size="lg" />
-                </a>
+                <div className={styles.socials}>
+                    <a href="https://www.linkedin.com/in/jake-st-germain/">
+                        <FontAwesomeIcon icon={faLinkedinIn} size="lg" />
+                    </a>
 
-                <a href="https://www.tiktok.com/@jakesaintg">
-                    <FontAwesomeIcon icon={faTiktok} size="lg" />
-                </a>
+                    <a href="https://github.com/JakeSaintG">
+                        <FontAwesomeIcon icon={faGithub} size="lg" />
+                    </a>
 
-                <a href="https://bsky.app/profile/jakesaintg.bsky.social">
-                    <FontAwesomeIcon icon={faBluesky} size="lg" />
-                </a>
+                    <a href="https://www.tiktok.com/@jakesaintg">
+                        <FontAwesomeIcon icon={faTiktok} size="lg" />
+                    </a>
 
-                <FontAwesomeIcon
-                    icon={faGear}
-                    size="lg"
-                    onClick={props.toggleSettings}
-                    style={{ cursor: 'pointer' }}
-                />
+                    <a href="https://bsky.app/profile/jakesaintg.bsky.social">
+                        <FontAwesomeIcon icon={faBluesky} size="lg" />
+                    </a>
+
+                    <FontAwesomeIcon
+                        icon={faGear}
+                        size="lg"
+                        onClick={props.toggleSettings}
+                        style={{ cursor: 'pointer' }}
+                        />
+                </div>
+
             </div>
-
             <button>
                 <FontAwesomeIcon icon={faBars} />
             </button>
