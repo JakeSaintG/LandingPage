61d2bb4fa462386f49afeec9d6f099398289cb38
JakeSaintG <jakesaint27@gmail.com>
2025-02-19 19:46:29 -0500

    start refactor contacts section

--- src/LandingPage/ContactSection/ContactSection.tsx
+++ src/LandingPage/ContactSection/ContactSection.tsx
@@ -1,8 +1,10 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import PageSection from '../../GeneralComponents/PageSection';
 import styles from './ContactSection.module.css';
+import { faBluesky, faTiktok } from '@fortawesome/free-brands-svg-icons';
 
-interface Props extends React.HTMLAttributes<HTMLElement>{
-    visualMode: string
+interface Props extends React.HTMLAttributes<HTMLElement> {
+    visualMode: string;
 }
 
 const contentStyle: React.CSSProperties = {};
@@ -10,10 +12,33 @@ const contentStyle: React.CSSProperties = {};
 // Just make this a footer. No need for a whole section (refactor)
 export default function ContactSection(props: Props) {
     const { visualMode } = props;
-    
+
     return (
-        <PageSection title='Contact' id="contactSection" style={contentStyle} visualMode={visualMode}>
-            <p>Placeholder</p>
-        </PageSection>
+        // <PageSection title='Contact' id="contactSection" style={contentStyle} visualMode={visualMode}>
+        //     <p>Placeholder</p>
+        // </PageSection>
+
+        <footer>
+            <div className={styles.socials}>
+                <h3>Socials</h3>
+                <div>
+                    <a href="https://www.tiktok.com/@jakesaintg" target="#">
+                        <FontAwesomeIcon icon={faTiktok} size="lg" />
+                    </a>
+
+                    <a href="https://bsky.app/profile/jakesaintg.bsky.social" target="#">
+                        <FontAwesomeIcon icon={faBluesky} size="lg" />
+                    </a>
+                </div>
+            </div>
+
+            <div className={styles.contact}>
+                <h3>Contact</h3>
+                <p>jake@stgermain.dev</p>
+            </div>
+
+            {/* <div className={styles.foo}>foobar</div> */}
+            <div className={styles.footer_base}>1993 Jake St. Germain</div>
+        </footer>
     );
 }
