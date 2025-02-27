aaa5ffe6d13ab2ddb39de3b041c6203348b4c609
Jake St. Germain <jakesaint27@gmail.com>
2025-02-27 00:00:18 -0500

    tweak blog todos and placeholders

--- src/LandingPage/BlogSection/BlogSection.tsx
+++ src/LandingPage/BlogSection/BlogSection.tsx
@@ -5,14 +5,13 @@ interface Props extends React.HTMLAttributes<HTMLElement>{
     visualMode: string
 }
 
-const contentStyle: React.CSSProperties = {};
-
+// TODO: The page will randomly scroll down when on the blog section. Find out why.
 export default function ContactSection(props: Props) {
     const { visualMode } = props;
     
     return (
-        <PageSection title='Blog' id='blogSection' style={contentStyle} visualMode={visualMode}>
-            <p>Placeholder</p>
+        <PageSection title='Blog' id='blogSection' style={{}} visualMode={visualMode}>
+            <p className={styles.placeholder}>Placeholder</p>
         </PageSection>
     );
 }
