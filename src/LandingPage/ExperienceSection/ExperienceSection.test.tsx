19f27ebed201b4d175b5461e8a00df260d0b989b
Jake St. Germain <jakesaint27@gmail.com>
2025-02-27 00:02:58 -0500

    experience section major refactor and clean up

--- src/LandingPage/ExperienceSection/ExperienceSection.test.tsx
+++ src/LandingPage/ExperienceSection/ExperienceSection.test.tsx
@@ -5,7 +5,6 @@ import { describe, it, test, expect, afterEach } from 'vitest';
 test('renders text from experience section', () => {
     render(<ExperienceSection visualMode="" />);
 
-    // TODO: Should this be tested? Probably not.
     const linkElement = screen.getByText(/Experience/i);
     expect(linkElement).toBeInTheDocument();
 });
--- src/LandingPage/ExperienceSection/ExperienceSection.tsx
+++ src/LandingPage/ExperienceSection/ExperienceSection.tsx
@@ -7,44 +7,50 @@ interface Props extends React.HTMLAttributes<HTMLElement>{
     visualMode: string
 }
 
-const contentStyle: React.CSSProperties = {};
+interface work {
+    title: string;
+    id: string;
+    current: boolean;
+    contributions: string[];
+}
+
+// TODO: REFACTOR! I'm sorting the array twice with both of these reduce functions.
+// A single function to sort 2 work exp buckets would be better.
+const currentWorkExp = workExperience.reduce( (acc, work) => {
+    if(work.current) acc.push(work);
+    return acc;
+}, [] as work[] );
+
+const prevWorkExp = workExperience.reduce( (acc, work) => {
+    if(!work.current) acc.push(work);
+    return acc;
+}, [] as work[] );
 
 export default function ExperienceSection(props: Props) {
-    const { visualMode } = props;
-    let [workHighlights, setWorkHighlights] = useState(workExperience[0].highlights);
-    let [activeClass, setActiveClass] = useState('dev-fulltime');
+    const {visualMode} = props;
+    const [workContributions, setWorkContributions] = useState(workExperience[0].contributions);
+    const [activeClass, setActiveClass] = useState('dev-fulltime');
     let details_key = 0;
     let previous_key = 0;
     let current_key = 0;
 
-    // TODO: Do these need to be in a useState()? Probably?
-    const currentWorkExp = workExperience.reduce( (acc, work) => {
-        if(work.current) acc.push(work);
-        return acc;
-    }, [] as any[] );
-
-    const prevWorkExp = workExperience.reduce( (acc, work) => {
-        if(!work.current) acc.push(work);
-        return acc;
-    }, [] as any[] );
-
     const returnWorkExperience = (event: MouseEvent<HTMLElement>) => {
-        setWorkHighlights(workHighlights = workExperience.find(e => e.id === event.currentTarget.id)!.highlights);
-        setActiveClass(activeClass = event.currentTarget.id);
+        setWorkContributions(workExperience.find(e => e.id === event.currentTarget.id)!.contributions);
+        setActiveClass(event.currentTarget.id);
     }
 
     return (
-        <PageSection title='Experience' id="experienceSection" style={contentStyle} visualMode={visualMode}>
+        <PageSection title='Experience' id="experienceSection" style={{}} visualMode={visualMode}>
             <div className={styles.experience_content}>
                 <div className={`${styles.experience_details} ${styles[props.visualMode]}`}>
-                    <ul>{workHighlights.map(e => <li key={details_key++}>{e}</li>)}</ul>
+                    <ul>{workContributions.map(e => <li key={details_key++}>{e}</li>)}</ul>
                 </div>
                 <div className={styles.experience}>
                     <h3 className={styles.current_header}>Current:</h3>
                     <div className={styles.current_experience}>
-                        {currentWorkExp.map((e: any) =>
+                        {currentWorkExp.map((e: work) =>
                             <button 
-                                key={previous_key++}
+                                key={current_key++}
                                 className={`${styles[props.visualMode]} ${activeClass === e.id ? `${styles.selected_profession} ${styles[props.visualMode]}` : `${styles.profession}`}`}
                                 id={e.id}
                                 onClick={returnWorkExperience}
@@ -56,7 +62,7 @@ export default function ExperienceSection(props: Props) {
 
                     <h3 className={styles.past_header}>Previous:</h3>
                     <div className={styles.past_experience}>
-                        {prevWorkExp.map((e: any) =>
+                        {prevWorkExp.map((e: work) =>
                             <button 
                                 key={previous_key++}
                                 className={`${styles[props.visualMode]} ${activeClass === e.id ? `${styles.selected_profession} ${styles[props.visualMode]}` : `${styles.profession}`}`}
