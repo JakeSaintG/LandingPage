ebdea3a762909c3712afb112b6685e75bd702840
Jake St. Germain <jakesaint27@gmail.com>
2025-02-26 20:48:39 -0500

    placeholder tests updates

--- src/ContactFooter/ContactFooter.test.tsx
+++ src/ContactFooter/ContactFooter.test.tsx
@@ -4,7 +4,7 @@ import { describe, it, test, expect, afterEach } from 'vitest';
 
 test('renders text from contact section', () => {
     render(<ContactSection visualMode='' />);
-    const linkElement = screen.getByText(/Placeholder/i);
+    const linkElement = screen.getByText(/Socials/i);
     expect(linkElement).toBeInTheDocument();
 });
 
