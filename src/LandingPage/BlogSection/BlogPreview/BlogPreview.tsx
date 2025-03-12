79601ffe9a0c18fa419fd704615b43242e3512e2
JakeSaintG <jakesaint27@gmail.com>
2025-03-12 19:34:58 -0400

    continued work on blog prevew

--- src/LandingPage/BlogSection/BlogPreview/BlogPreview.tsx
+++ src/LandingPage/BlogSection/BlogPreview/BlogPreview.tsx
@@ -1,15 +1,20 @@
 import styles from './BlogPreview.module.css';
 
-interface Props extends React.HTMLAttributes<HTMLElement>{
-    visualMode: string
+interface Props extends React.HTMLAttributes<HTMLElement> {
+    visualMode: string;
 }
 
 export default function BlogPreview(props: Props) {
     return (
-        <div className={`${styles.placeholder_blog_preview} ${styles[props.visualMode]}`}>
+        <div className={`${styles.blog_preview} ${styles[props.visualMode]}`}>
             <h3>Title</h3>
             <p>date</p>
-            <p>Preview? Maybe?</p>
+            <div className={styles.tags}>
+                <p>Tag</p>
+                <p>Tag</p>
+                <p>Tag</p>
+                <p>Tag</p>
+            </div>
         </div>
-    )
+    );
 }
