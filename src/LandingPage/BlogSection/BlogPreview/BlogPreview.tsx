af81872dbc88b7d6af964cba48933f5ec97f53fe
JakeSaintG <jakesaint27@gmail.com>
2025-03-24 00:06:14 -0400

    tags map

--- src/LandingPage/BlogSection/BlogPreview/BlogPreview.tsx
+++ src/LandingPage/BlogSection/BlogPreview/BlogPreview.tsx
@@ -1,5 +1,5 @@
 import styles from './BlogPreview.module.css';
-import blog from '../BlogSection'
+import blog from '../BlogSection';
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
     visualMode: string;
@@ -8,25 +8,28 @@ interface Props extends React.HTMLAttributes<HTMLElement> {
 }
 
 export interface blog {
-    title: string,
-    author: string,
-    publish_date: string,
-    tags: string[],
-    markdown: string,
-    visible: boolean
+    title: string;
+    author: string;
+    publish_date: string; // Should this be a timestamp? Might be more performant to just store it as a date.
+    tags: string[];
+    markdown: string;
+    visible: boolean;
 }
 
 export default function BlogPreview(props: Props) {
     return (
-        <div className={`${styles.blog_preview} ${styles[props.visualMode]}`} onClick={props.displayBlog}>
+        <div
+            className={`${styles.blog_preview} ${styles[props.visualMode]}`}
+            onClick={props.displayBlog}
+        >
             <h3>{props.blog.title}</h3>
+            {/* TODO: Parse the date... */}
             <p>{props.blog.publish_date}</p>
-
             <div className={styles.tags}>
-                <p>Tag</p>
-                <p>Tag</p>
-                <p>Tag</p>
-                <p>Tag</p>
+                {/* TODO: Style this to allow 2 rows of 3-4 tags */}
+                {props.blog.tags.map((tag) => (
+                    <p>{tag}</p>
+                ))}
             </div>
         </div>
     );
