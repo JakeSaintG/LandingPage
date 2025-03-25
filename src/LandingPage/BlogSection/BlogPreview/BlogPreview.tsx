61b7414bbceac05793e28913d5e8176f0d8203dc
JakeSaintG <jakesaint27@gmail.com>
2025-03-24 21:16:15 -0400

    continued work on blog preview

--- src/LandingPage/BlogSection/BlogPreview/BlogPreview.tsx
+++ src/LandingPage/BlogSection/BlogPreview/BlogPreview.tsx
@@ -10,25 +10,30 @@ interface Props extends React.HTMLAttributes<HTMLElement> {
 export interface blog {
     title: string;
     author: string;
-    publish_date: string; // Should this be a timestamp? Might be more performant to just store it as a date.
+    publish_date: string;
     tags: string[];
     markdown: string;
     visible: boolean;
 }
 
 export default function BlogPreview(props: Props) {
+    const postDate = new Date(parseInt(props.blog.publish_date) * 1000).toDateString();
+    let key = 0;
+    let tags = props.blog.tags;
+
+    if (tags.length >= 6 ) tags = tags.slice(0, 6);
+
     return (
         <div
             className={`${styles.blog_preview} ${styles[props.visualMode]}`}
             onClick={props.displayBlog}
         >
             <h3>{props.blog.title}</h3>
-            {/* TODO: Parse the date... */}
-            <p>{props.blog.publish_date}</p>
+            <p>{postDate}</p>
             <div className={styles.tags}>
                 {/* TODO: Style this to allow 2 rows of 3-4 tags */}
-                {props.blog.tags.map((tag) => (
-                    <p>{tag}</p>
+                {tags.map((tag) => (
+                    <p key={key++}>{tag}</p>
                 ))}
             </div>
         </div>
